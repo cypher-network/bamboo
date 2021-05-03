8e15f1ffcf91495580c9538d6d667348319ae573
pingpongsneak <matthew.hellyer@pm.me>
2021-05-03 09:52:17 +0100

    refactor: balance check spinner

--- cli/ApplicationLayer/Commands/Wallet/WalletBalanceCommand.cs
+++ cli/ApplicationLayer/Commands/Wallet/WalletBalanceCommand.cs
@@ -15,6 +15,7 @@ using McMaster.Extensions.CommandLineUtils;
 
 using BAMWallet.Extentions;
 using BAMWallet.HD;
+using Kurukuru;
 
 namespace CLi.ApplicationLayer.Commands.Wallet
 {
@@ -24,32 +25,35 @@ namespace CLi.ApplicationLayer.Commands.Wallet
         private readonly IWalletService _walletService;
         private readonly IConsole _console;
 
+        private Spinner _spinner;
+        
         public WalletBalanceCommand(IServiceProvider serviceProvider)
         {
             _walletService = serviceProvider.GetService<IWalletService>();
             _console = serviceProvider.GetService<IConsole>();
         }
 
-        public override Task Execute()
+        public override async Task Execute()
         {
             try
             {
                 using var identifier = Prompt.GetPasswordAsSecureString("Identifier:", ConsoleColor.Yellow);
                 using var passphrase = Prompt.GetPasswordAsSecureString("Passphrase:", ConsoleColor.Yellow);
-
-                var session = _walletService.SessionAddOrUpdate(new Session(identifier, passphrase));
-                var total = _walletService.AvailableBalance(session.SessionId);
-
-                _console.ForegroundColor = ConsoleColor.Green;
-                _console.WriteLine($"\nBalance: {total.Result.DivWithNaT():F9}\n");
-                _console.ForegroundColor = ConsoleColor.White;
+                await Spinner.StartAsync("Checking balance ...", spinner =>
+                {
+                    _spinner = spinner;
+                    var session = _walletService.SessionAddOrUpdate(new Session(identifier, passphrase));
+                    var total = _walletService.AvailableBalance(session.SessionId);
+                    _console.ForegroundColor = ConsoleColor.Green;
+                    _console.WriteLine($"\nBalance: {total.Result.DivWithNaT():F9}\n");
+                    _console.ForegroundColor = ConsoleColor.White;
+                    return Task.CompletedTask;
+                });
             }
             catch (Exception)
             {
                 throw;
             }
-
-            return Task.CompletedTask;
         }
     }
 }
