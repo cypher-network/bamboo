4f336b014a673c95afef6acfb37387961c0f831b
Stefanos Anastasiou <stefanos.anastasiou@esrlabs.com>
2021-06-11 00:59:35 +0200

    Finalize installer.

--- install/windows/BambooWalletInstaller/path.ps1
+++
@@ -1,3 +0,0 @@
-$newPath = 'C:\Program Files\Cypher\Wallet\Bamboo'
-$oldPath = [Environment]::GetEnvironmentVariable('PATH', 'Machine');
-[Environment]::SetEnvironmentVariable('PATH', "$newPath;$oldPath",'Machine');
