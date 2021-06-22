fd4682ec2e922c542661bf83ef4b0798f0441b1e
pingpongsneak <matthew.hellyer@pm.me>
2021-06-22 22:26:16 +0100

    fix: endless loop when no transaction in wallet

--- bamwallet/HD/WalletService.cs
+++ bamwallet/HD/WalletService.cs
@@ -1430,6 +1430,7 @@ namespace BAMWallet.HD
                     {
                         _logger.LogError(ex.Message);
                         _resetEvent.Set();
+                        break;
                     }
                 }
             });
