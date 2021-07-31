eb2d2bcfb7029a7045d7afaf8fe3bd0aa3d8d67d
Stephan Kempkes <stephan.kempkes@gmail.com>
2021-07-31 17:09:24 +0200

    refactor: remove unused constants

--- bamwallet/HD/Constant.cs
+++ bamwallet/HD/Constant.cs
@@ -5,18 +5,8 @@ namespace BAMWallet.HD
 {
     public static class Constant
     {
-        public const string Network = "network";
-        public const string RunAsWebServer = "run_as_web_server";
-        public const string RemoteNode = "remote_node";
-        public const string Routing = "routing";
-        public const string Environment = "environment";
-        public const string Advertise = "advertise";
         public const string Mainnet = "mainnet";
         public const string Testnet = "testnet";
-        public const string NumberOfConfirmations = "number_of_confirmations";
-        public const int Tan = 1;
-        public const int MicroTan = 100;
         public const int NanoTan = 1000_000_000;
-        public const ulong AttoTan = 1000_000_000_000_000_000;
     }
 }
